418f0bf8ba10acddbd87e3fccc5e63f22ec10d3b
mangtaeeee <osp9658@naver.com>
2023-04-14 18:11:04 +0900

    [04.14]마무리

--- src/main/java/com/codeum/shoppingmall/admin/viewcontroller/AdminViewController.java
+++ src/main/java/com/codeum/shoppingmall/admin/viewcontroller/AdminViewController.java
@@ -1,4 +1,15 @@
 package com.codeum.shoppingmall.admin.viewcontroller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AdminViewController {
+
+
+    @GetMapping("/admin/store")
+    public String mainPage(){
+        return "admin-main";
+    }
 }
